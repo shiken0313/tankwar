e09cfc900dca98583ff38f6920cded34fa239a01
shiken <sro800313@gmail.com>
2020-04-21 14:36:18 +0900

    顯示主介面和我方坦克

---
+++ src/test/java/com/javaneversleep/tankwar/GameClient.java
@@ -0,0 +1,29 @@
+package com.javaneversleep.tankwar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient  extends JComponent {
+
+    private GameClient(){
+        this.setPreferredSize(new Dimension(800 ,600));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),400,100,null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("史上最無聊的坦克大戰");//JFrame frame=new JFrame(String "titlename");//gui實例
+        //frame.setTitle("史上最無聊的坦克大戰");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        GameClient client = new GameClient();
+        frame.add(client);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//退出程序
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
